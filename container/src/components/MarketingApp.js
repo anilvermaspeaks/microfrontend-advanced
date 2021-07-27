d046dd1ac40d22f95eacc7319ad80fa86a558f86
anil verma <anilcs240@gmail.com>
2021-07-27 08:07:57 +0530

    handling routing in microfrontend

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,12 +1,25 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom'
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathName }) => {
+        const { pathName } = history.location;
+        if (pathName !== nextPathName) {
+          history.push(nextPathName)
+        }
+      }
+    });
+
+    if (onParentNavigate) {
+      history.listen(onParentNavigate)
+    }
+  }, []);
 
   return <div ref={ref} />;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history'
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNaviagte, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNaviagte) {
+    history.listen(onNaviagte)
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathName } = history.location;
+      if (pathName !== nextPathName) {
+        history.push(nextPathName)
+      }
+    }
+  }
 };
 
 // If we are in development and in isolation,
@@ -13,7 +28,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
